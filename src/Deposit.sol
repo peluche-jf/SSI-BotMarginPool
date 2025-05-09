93cad1f1ea67f181c767a196f174a1e3134ac9a7
peluche-jf <peluche.jf@icloud.com>
2025-05-09 18:18:00 -0300

    Update LPManager and Deposit contracts with POL deposit handling and tax functionality

--- src/Deposit.sol
+++ src/Deposit.sol
@@ -1,1 +1,24 @@
-// SPDX 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./LPManager.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Deposit is LPManager {
+
+    
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        tax(msg.value);
+        
+        
+
+    }
+
+    function tax() public payable {
+        require(msg.value > 0, "Comission not enough");
+        uint256 comission = msg.value*99/100;
+        owner.transfer(comission);
+        return msg.value -= comission;
+    }
+}
